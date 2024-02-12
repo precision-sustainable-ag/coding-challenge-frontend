a7773e70c93d7e7602bb9030adc1c9b1c10430e6
Mikah Pinegar <55995241+mikahpinegar@users.noreply.github.com>
2024-02-12 15:00:28 -0500

    add initial files

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import About from "./About";
+import License from "./License";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<About />}></Route>
+        <Route path="/license" element={<License />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
